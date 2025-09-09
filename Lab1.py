98372305ee9661e80731bf35e46ae941a2dd6189
Adin Ashby <adin.ashby@gmail.com>
2025-09-09 10:17:33 -0400

    Solved Q1

--- Lab1.py
+++ Lab1.py
@@ -2,10 +2,13 @@
 # This function should print "Hello, World!" to the screen.
 def hello_world():
     print("Hello, World!")
-
+    
 # Function 2: Get input and output with different variable types
 # This function should prompt the user for their name (string), age (int), and height (float),
 # and then print them back in a formatted message.
 def input_output():
     # TODO: Implement this function
     pass  # Replace with your code
+
+# hello_world()
+# input_output()
